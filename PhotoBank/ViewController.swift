b7e764797e5d38f65a23be0ced802c0e5f43e2e2
Marquise Kamanke <mkamanke@deloitte.ca>
2020-02-09 12:40:28 -0500

    Initial Commit

---
+++ PhotoBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoBank
+//
+//  Created by Marquise Kamanke on 2020-02-09.
+//  Copyright © 2020 Marquise Kamanke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
